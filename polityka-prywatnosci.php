082254a9ea4e284826173178996153be1313551e
picabela <kontakt@picabela.pl>
2025-11-13 14:15:17 +0100

    Update contact phone number in privacy policy

--- polityka-prywatnosci.php
+++ polityka-prywatnosci.php
@@ -137,7 +137,7 @@ include 'includes/header.php';
                     <p>W przypadku pytań dotyczących przetwarzania danych osobowych lub niniejszej polityki prywatności, prosimy o kontakt:</p>
                     <ul>
                         <li><strong>Email:</strong> <a href="mailto:kontakt@butolog.pl">kontakt@butolog.pl</a></li>
-                        <li><strong>Telefon:</strong> +48 507 616 514/li>
+                        <li><strong>Telefon:</strong> +48 515 435 653/li>
         
                     </ul>
                 </div>
